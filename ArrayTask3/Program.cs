5fa2a875e423d287320531098c32c507c4603277
Elitsa Georgeva <Elitsa Georgeva@DESKTOP-8D1ST4F>
2023-09-19 21:52:21 +0300

    Add project files.

---
+++ ArrayTask3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of elements in the array: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter the elements of the array:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Element {i + 1}: ");
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        int sum = 0;
+
+        // Calculate the sum of all elements in the array
+        for (int i = 0; i < n; i++)
+        {
+            sum += arr[i];
+        }
+
+        Console.WriteLine($"Sum of all elements in the array: {sum}");
+    }
+}
+
